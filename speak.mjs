e44e1666e8c979f0543b2b5999920863842e07a3
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-14 18:36:09 +0900

    Node.js のパイプは socketpairなので、/dev/stdoutを指定すると上手くいかない。

--- speak.mjs
+++ speak.mjs
@@ -1,15 +1,28 @@
 import {spawn} from "child_process";
-
-// echo "こんにちは" | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow ./hello.wav
+// echo "こんにちは" | open_jtalk -x /var/lib/mecab/dic/open-jtalk/naist-jdic -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice -ow /dev/stdout | aplay --quiet
 
 export function speak(text){
-  const jtalk = spawn('open_jtalk', [
-    '-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic',
-    '-m', '/usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice'
-  ]);
-  const aplay = spawn('aplay');
-  jtalk.stdout.pipe(aplay.stdin);
-  jtalk.stdin.write(text);
-  jtalk.stdin.end();
+  // TODO: quote
+  const safeText = text;
+
+  const cmd = [
+    `echo ${safeText}`,
+    "|",
+    "open_jtalk",
+    "-x /var/lib/mecab/dic/open-jtalk/naist-jdic",
+    "-m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice",
+    "-ow /dev/stdout",
+    "|",
+    "aplay --quiet"
+  ].join(" ");
+
+  // /dev/stdoutを使うとpipeで処理できなかったのでshellで実行する
+  const proc = spawn(cmd, {
+    shell: true
+  });
+
+  proc.on("error", err => {
+    console.error("speakShell error:", err);
+  });
 }
 
